f99289ddbad122f4ffd57731863b5ba3da5a38b8
Beatriz Cristina Luiz - 705011453 <beatriz.luiz@animaeducacao.com.br>
2021-08-18 09:51:52 -0300

    refactoring

--- imagedetection.py
+++ imagedetection.py
@@ -1,36 +1,41 @@
 import cv2
 import mediapipe as mp
 
-# opencv - integrar webcam e usar python para trabalhar imagem
-# mediapipe - reconhecer as mãos usando solutions hands
+# conect the webcam to python
+webcam = cv2.VideoCapture(0) # starts a conection with the webcam
 
+# inicializing the mediapipe
+hands_recogner = mp.solutions.hands
+draw_mp = mp.solutions.drawing_utils
+hands = hands_recogner.Hands()
 
+# what happen if the webcam inst open
+if webcam.isOpened():
+    # read the webcam (webcam.read())
+    validate, frame = webcam.read()
+    # Undestand webcam.read() -> get the frames
+    # loop 
+    while validate:
+        # get the next frame
+        validate, frame = webcam.read()
+        
+        # convert BGR (opencv pattern) to RGB (mediapipe pattern)
+        frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        # Draw the hands
+        hands_list = hands.process(frameRGB)
+        if hands_list.multi_hand_landmarks:
+            for mao in hands_list.multi_hand_landmarks:
+                draw_mp.draw_landmarks(frame, hands, hands_recogner.HAND_CONNECTIONS)
 
-webcam = cv2.VideoCapture(0) # cria uma conexão com a web cam, o número 0 significa que estou usando a camera zero (câmera nativa do notebook)
-
-reconhecimento_maos = mp.solutions.hands #inicializando mediapipe - solution hands: Reconhecimento de mãos
-desenho_mp = mp.solutions.drawing_utils
-maos = reconhecimento_maos.hands
-
-if webcam.isOpened(): # o que acontece se a camera não abrir 
-    validacao, frame = webcam.read()
-    while validacao: #loop infinito
-        validacao, frame = webcam.read()
-
-        frameRGB = cv2.cvtColor(frame, cv2.COLOR.BRG2RGB) # converte formato bgr para rgb
-        # desenhar a mão
-        lista_maos = maos.process(frameRGB)
-        if lista_maos.multi_hand_landmarks:
-            for mao in lista_maos.multi_hand_landmarks:
-                print(mao.landmark)
-                desenho_mp.draw_landmarks(frame, mao, reconhecimento_maos.HAND_CONNECTIONS)
-
-
+        # show the frame of webcam that python is looking
         cv2.imshow("Video", frame)
-        tecla = cv2.waitKey(2) # 60 segundos -> 2 milisegundos = 30fps
-        
-        if tecla == 27: # fazer o código parar -- esc é o 27 na tabela scsi
+        # ask to python wait -> in a smart way xD 
+        key = cv2.waitKey(2)
+        # ask to stop when you press the key 27 is ESC 
+        if key == 27:
             break
-            
-        
-webcam.release() # Fecha a webcam
+
+#  Stop
+webcam.release()
+cv2.destroyAllWindows()
